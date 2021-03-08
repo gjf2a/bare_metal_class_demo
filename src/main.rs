69ae47866b58d48f490b3a7a1f03a1d95c3b6e40
gjf2a <ferrer@hendrix.edu>
2021-03-08 10:42:26 -0600

    A bare-metal executable that compiles and runs!

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            let char_offset = i as isize * 2;
+            let color_offset = char_offset + 1;
+            *vga_buffer.offset(char_offset) = byte;
+            *vga_buffer.offset(color_offset) = 0xb;
+        }
+    }
     loop {}
 }
 
